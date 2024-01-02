e470bbf48fcfc37796ef1a44f274287d0f720501
Jaroslav Týc <mail@jaroslavtyc.com>
2024-01-02 12:34:17 +0100

    First version of upgrade to Sylius 1.11 and 1.12

--- src/DependencyInjection/ThreeBRSSyliusZasilkovnaExtension.php
+++ src/DependencyInjection/ThreeBRSSyliusZasilkovnaExtension.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace ThreeBRS\SyliusZasilkovnaPlugin\DependencyInjection;
 
+use Sylius\Bundle\ResourceBundle\DependencyInjection\Extension\AbstractResourceExtension;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ThreeBRSSyliusZasilkovnaExtension extends Extension
+class ThreeBRSSyliusZasilkovnaExtension extends AbstractResourceExtension
 {
+    /** @psalm-suppress UnusedVariable */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
 }
