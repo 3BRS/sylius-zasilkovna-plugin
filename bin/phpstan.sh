21256a4beb2edf0979d4b41c1a5d73b1d84b5436
Jaroslav Týc <mail@jaroslavtyc.com>
2025-05-26 15:58:09 +0200

    SLS-5 Fix tests

--- bin/phpstan.sh
+++ bin/phpstan.sh
@@ -7,7 +7,15 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$(dirname "$DIR")"
 
 set -x
-tests/Application/bin/console cache:warmup --no-optional-warmers
+
+# to avoid crash
+# [PHPStan\Symfony\XmlContainerNotExistsException]
+# Container /src/var/cache/dev/App_KernelDevDebugContainer.xml does not exist
+# (note: /src/var/cache/dev/srcApp_KernelDevDebugContainer.xml is legacy path)
+if [ ! -f var/cache/dev/App_KernelDevDebugContainer.xml ] && [ ! -f var/cache/dev/srcApp_KernelDevDebugContainer.xml ]; then
+  php bin/console --env=dev cache:warmup --no-optional-warmers
+fi
+
 XDEBUG_MODE=off php -d memory_limit=1G vendor/bin/phpstan analyse \
     --debug \
     --level max \
